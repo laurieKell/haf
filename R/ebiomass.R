88313a15db5d0331b9ebadd2cbc27456e013fb20
lauriekell <laurie@kell.es>
2025-07-05 08:16:21 +0100

    added eql

--- R/ebiomass.R
+++ R/ebiomass.R
@@ -1,60 +1,63 @@
 #' Calculate Exploitable Biomass
 #'
 #' @description
-#' Calculates the exploitable biomass from an FLStock object using selectivity-weighted
-#' catch weights and stock numbers.
+#' Calculates the exploitable biomass from FLR objects using selectivity-weighted
+#' catch weights and stock numbers. Exploitable biomass represents the biomass
+#' that is vulnerable to fishing, weighted by the selectivity pattern.
 #'
-#' @param object An object of class FLStock
+#' @param object An object of class FLStock or FLBRP
 #'
 #' @return An FLQuant object containing the exploitable biomass time series
 #'
 #' @details
-#' Exploitable biomass is based on weighting catch weights by selectivity normalised 
-#' by peak selectivity, then multiplying by stock numbers and summing across ages
+#' Exploitable biomass is calculated by:
+#' 1. Weighting catch weights by the selectivity pattern
+#' 2. Normalizing by the peak selectivity (fapex)
+#' 3. Multiplying by stock numbers
+#' 4. Summing across ages
+#'
+#' The formula used is: \code{sum(catch.wt * selectivity / fapex(selectivity) * stock.n)}
 #'
 #' @examples
 #' \dontrun{
 #' data(ple4)
 #' eb <- ebiomass(ple4)
+#' plot(eb)
 #' }
 #'
 #' @export
 setGeneric("ebiomass", function(object) standardGeneric("ebiomass"))
 
-#' Calculate total mortality-at-age (Z) using length data.
-#' 
-#' This is a generic S4 method that calculates something using the `haupt` function.
-#' The specific implementation depends on the class of the input object.
-#' 
-#' @param object An object of class FLQuant or data.frame (depends on the method).
-#' @param pars A parameter object (class FLPar) containing necessary parameters.
-#' @param lc A threshold value for lc.
-#' @param lmax A threshold value for lmax.
-#' @param ... Additional arguments .
-#' 
-#' @return estimates of Z.
-#' 
-#' @export
-setGeneric("haupt", function(object, pars, lc, lmax, ...) {
-  standardGeneric("haupt")
-})
-
 #' @rdname ebiomass
 #' @export
 setMethod("ebiomass", signature(object="FLStock"),
           function(object) {
-            sel   <- harvest(object)
-            wt    <- catch.wt(object) %*% sel %/% fapex(sel)
+            # Get selectivity pattern
+            sel <- harvest(object)
+            
+            # Weight catch weights by selectivity and normalize by peak
+            wt <- catch.wt(object) %*% sel %/% fapex(sel)
+            
+            # Ensure minimum weight to avoid numerical issues
             eb.wt <- qmax(wt, 0.000001)
             
+            # Calculate exploitable biomass by summing across ages
             apply(eb.wt %*% stock.n(object), 2:6, sum)
           })
 
+#' @rdname ebiomass
+#' @export
 setMethod("ebiomass", signature(object="FLBRP"),
           function(object) {
-            sel   <- harvest(object)
-            wt    <- catch.wt(object) %*% sel %/% fapex(sel)
+            # Get selectivity pattern
+            sel <- harvest(object)
+            
+            # Weight catch weights by selectivity and normalize by peak
+            wt <- catch.wt(object) %*% sel %/% fapex(sel)
+            
+            # Ensure minimum weight to avoid numerical issues
             eb.wt <- qmax(wt, 0.000001)
             
+            # Calculate exploitable biomass by summing across ages
             apply(eb.wt %*% stock.n(object), 2:6, sum)
           })
---
+++ R/eql.R
@@ -0,0 +1,64 @@
+#' @title Equilibrium Model Fitting
+#' @description Fits stock-recruitment models and computes biological reference points using FLR framework.
+#'
+#' @param object An FLStock object
+#' @param model Stock-recruitment model type (default="bevholtSV")
+#' 
+#' @return An FLBRP object with additional attributes:
+#' \itemize{
+#'   \item sr - Fitted stock-recruitment model
+#'   \item logLik - Model likelihood
+#'   \item prod - Production characteristics
+#'   \item tseries - Time series metrics
+#' }
+#' 
+#' @examples
+#' \dontrun{
+#' data(ple4)
+#' eql <- eql(ple4, model="rickerSV")
+#' summary(eql)
+#' }
+#' 
+#' @rdname eql
+#' @export
+setGeneric("eql", function(object, model) standardGeneric("eql"))
+
+#' @rdname eql
+#' @export
+setMethod("eql", signature(object="FLStock"),
+          function(object,model="bevholtSV"){
+            
+            spFn<-function(x){
+              rfs=FLPar(c(ssb.obs(x)),dimnames=list(refpts="ssb",
+                                                    quant =dimnames(refpts(x))$quant,
+                                                    iter  =seq(dim(ssb.obs(x))[2])))
+              rfs[,-4]=NA
+              refpts(x)=rfs
+              
+              rtn=data.frame(model.frame(FLQuants(x,ssb=ssb.obs,catch=catch.obs),drop=TRUE),
+                             sp=c(computeRefpts(x)[,"yield"]))
+              rtn$pe=(c(rtn$ssb[-1]-rtn$ssb[-dim(rtn)[1]]+rtn$catch[-dim(rtn)[1]]-
+                          rtn$sp[-dim(rtn)[1]],NA))/rtn$ssb
+              
+              rtn}
+            
+            spr0=FLCandy:::spr0Yr(object)
+            sr  =as.FLSR(object,model=model)
+            sr  =FLCandy:::ftmb(sr,s.est    =T,
+                                s        =0.7, #fishlife(object)["s"],
+                                s.logitsd=0.4, #fishlife(object)["sd.logit.s"],
+                                spr0     =spr0)
+            
+            rtn=brp(FLBRP(object,nyears=dim(object)[2],
+                          sr=list(model =do.call(gsub("SV","", model),list())$model,
+                                  params=FLPar(apply(params(sr),1,median)))))
+            
+            attributes(rtn)[["sr"]]     =sr
+            attributes(rtn)[["logLik"]] =logLik(sr)
+            attributes(rtn)[["prod"]]   =spFn(rtn)
+            attributes(rtn)[["tseries"]]=tseries(object)
+            attributes(rtn)[["eb.obs"]] =ebiomass(object)
+            attributes(rtn)[["priors"]] =tryIt(calcPriors(rtn))
+            attributes(rtn)[["prior2"]] =tryIt(FLCandy:::getPriors(rtn))
+            
+            return(rtn)})
